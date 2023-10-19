f187803758a42b2ebb3a5620a32ae38d770bdffa
quanlahe153624 <quanlahe153624@fpt.edu.vn>
2023-10-19 22:59:03 +0700

    [Update] update with organization account can update

--- src/main/java/fpt/edu/capstone/vms/persistence/service/impl/SettingSiteMapServiceImpl.java
+++ src/main/java/fpt/edu/capstone/vms/persistence/service/impl/SettingSiteMapServiceImpl.java
@@ -3,11 +3,14 @@ package fpt.edu.capstone.vms.persistence.service.impl;
 import fpt.edu.capstone.vms.controller.ISettingSiteMapController;
 import fpt.edu.capstone.vms.persistence.entity.SettingSiteMap;
 import fpt.edu.capstone.vms.persistence.entity.SettingSiteMapPk;
+import fpt.edu.capstone.vms.persistence.entity.User;
 import fpt.edu.capstone.vms.persistence.repository.SettingRepository;
 import fpt.edu.capstone.vms.persistence.repository.SettingSiteMapRepository;
 import fpt.edu.capstone.vms.persistence.repository.SiteRepository;
+import fpt.edu.capstone.vms.persistence.repository.UserRepository;
 import fpt.edu.capstone.vms.persistence.service.ISettingSiteMapService;
 import fpt.edu.capstone.vms.persistence.service.generic.GenericServiceImpl;
+import fpt.edu.capstone.vms.util.SecurityUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -16,6 +19,7 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -24,19 +28,22 @@ public class SettingSiteMapServiceImpl extends GenericServiceImpl<SettingSiteMap
     private final SettingSiteMapRepository settingSiteMapRepository;
     private final SettingRepository settingRepository;
     private final SiteRepository siteRepository;
+    private final UserRepository userRepository;
     private final ModelMapper mapper;
 
 
-    public SettingSiteMapServiceImpl(SettingSiteMapRepository settingSiteMapRepository, SettingRepository settingRepository, SiteRepository siteRepository, ModelMapper mapper) {
+    public SettingSiteMapServiceImpl(SettingSiteMapRepository settingSiteMapRepository, SettingRepository settingRepository, SiteRepository siteRepository, UserRepository userRepository, ModelMapper mapper) {
         this.settingSiteMapRepository = settingSiteMapRepository;
         this.settingRepository = settingRepository;
         this.siteRepository = siteRepository;
+        this.userRepository = userRepository;
         this.mapper = mapper;
         this.init(this.settingSiteMapRepository);
     }
 
     @Override
     public SettingSiteMap createOrUpdateSettingSiteMap(ISettingSiteMapController.SettingSiteInfo settingSiteInfo) {
+
         if (ObjectUtils.isEmpty(settingSiteInfo)) {
             throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Object is null");
         }
@@ -48,11 +55,23 @@ public class SettingSiteMapServiceImpl extends GenericServiceImpl<SettingSiteMap
         Long settingId = Long.valueOf(settingSiteInfo.getSettingId());
         UUID siteId = UUID.fromString(settingSiteInfo.getSiteId());
 
-        if (!siteRepository.existsById(siteId))
+        var site = siteRepository.findById(siteId);
+        if (site.isEmpty())
             throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "SiteId is not correct in database!!");
 
         if (!settingRepository.existsById(settingId))
             throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "SettingId is not correct in database!!");
+
+        var user = userRepository.findByUsername(SecurityUtils.loginUsername());
+
+        if (!site.get().getOrganizationId().equals(UUID.fromString(SecurityUtils.getOrgId()))) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Please login with account of organization");
+        }
+
+        if (ObjectUtils.isEmpty(user) && !user.get().getDepartment().getSiteId().equals(siteId)) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Please login before change data setting!!");
+        }
+
 
         SettingSiteMapPk pk = new SettingSiteMapPk(settingId, siteId);
         SettingSiteMap settingSiteMap = settingSiteMapRepository.findById(pk).orElse(null);
