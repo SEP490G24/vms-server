43979f9be65d5a7e3f67a02dfd3c5a61f2632871
quanlahe153624 <quanlahe153624@fpt.edu.vn>
2023-10-29 01:23:12 +0700

    [Update] update

--- src/main/java/fpt/edu/capstone/vms/controller/ITemplateController.java
+++ src/main/java/fpt/edu/capstone/vms/controller/ITemplateController.java
@@ -9,7 +9,15 @@ import jakarta.ws.rs.QueryParam;
 import lombok.Data;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,7 +25,7 @@ import java.util.UUID;
 
 
 @RestController
-@Tag(name = "Room Service")
+@Tag(name = "Template Service")
 @RequestMapping("/api/v1/template")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface ITemplateController {
--- src/main/java/fpt/edu/capstone/vms/controller/ITicketController.java
+++ src/main/java/fpt/edu/capstone/vms/controller/ITicketController.java
@@ -4,12 +4,22 @@ import fpt.edu.capstone.vms.constants.Constants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
-import lombok.Builder;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -44,7 +54,6 @@ public interface ITicketController {
     ResponseEntity<?> create(@RequestBody @Valid CreateTicketInfo ticketInfo);
 
     @Data
-    @Builder
     class CreateTicketInfo {
 
         @NotNull(message = "The name of the visitor cannot be null")
