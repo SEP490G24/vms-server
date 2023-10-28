d0c6f821576132b020905f4f839edcd2a62d954f
quanlahe153624 <quanlahe153624@fpt.edu.vn>
2023-10-28 23:14:04 +0700

    [Update] update ticket

---
+++ src/main/java/fpt/edu/capstone/vms/persistence/entity/CustomerTicketMap.java
@@ -0,0 +1,50 @@
+package fpt.edu.capstone.vms.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Table(schema = "vms", name = "user_role_map")
+public class CustomerTicketMap extends AbstractBaseEntity<CustomerTicketMapPk> {
+
+    @EmbeddedId
+    private CustomerTicketMapPk customerTicketMapPk;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Customer customerEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Ticket ticketEntity;
+
+    public CustomerTicketMap update(CustomerTicketMap customerTicketMap) {
+        if (customerTicketMap.getCreatedBy() != null) this.setCreatedBy(customerTicketMap.getCreatedBy());
+        if (customerTicketMap.getCreatedOn() != null) this.setCreatedOn(customerTicketMap.getCreatedOn());
+        return this;
+    }
+
+    @Override
+    public void setId(CustomerTicketMapPk id) {
+        this.customerTicketMapPk = id;
+    }
+
+    @Override
+    public CustomerTicketMapPk getId() {
+        return this.customerTicketMapPk;
+    }
+}
--- src/main/java/fpt/edu/capstone/vms/persistence/entity/Ticket.java
+++ src/main/java/fpt/edu/capstone/vms/persistence/entity/Ticket.java
@@ -2,14 +2,18 @@ package fpt.edu.capstone.vms.persistence.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import fpt.edu.capstone.vms.constants.Constants;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKey;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +22,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Data
@@ -86,6 +91,11 @@ public class Ticket extends AbstractBaseEntity<UUID> {
     @JoinColumn(name = "template_id", referencedColumnName = "id", insertable = false, updatable = false)
     @JsonIgnore
     private Template template;
+
+    @OneToMany(mappedBy = "ticketEntity", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @MapKey(name = "customerTicketMapPk.ticketId")
+    @JsonIgnore
+    private Map<UUID, CustomerTicketMap> customerTicketMaps;
 
     @Override
     public UUID getId() {
