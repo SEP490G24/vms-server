ff08e310d605ef79e8caed0323328861d7396e8c
quanlahe153624 <quanlahe153624@fpt.edu.vn>
2023-10-29 01:51:06 +0700

    [Update] update max call

--- src/main/java/fpt/edu/capstone/vms/security/SecurityConfig.java
+++ src/main/java/fpt/edu/capstone/vms/security/SecurityConfig.java
@@ -12,9 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -60,7 +59,7 @@ public class SecurityConfig {
                                 .jwkSetUri(jwkSetUri)));
 
         httpSecurity
-            .addFilterBefore(ipRateLimitingFilter(), BasicAuthenticationFilter.class)
+            .addFilterBefore(ipRateLimitingFilter(), UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(permitAll).permitAll()
                 .anyRequest()
