97178f35a89af3b8fc81878de8356b0e96b05e69
quanlahe153624 <quanlahe153624@fpt.edu.vn>
2023-12-01 23:09:38 +0700

    [Update] update dashboard test

--- src/main/java/fpt/edu/capstone/vms/controller/ITicketController.java
+++ src/main/java/fpt/edu/capstone/vms/controller/ITicketController.java
@@ -22,7 +22,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -68,7 +67,7 @@ public interface ITicketController {
     @PreAuthorize("hasRole('r:ticket:cancel')")
     ResponseEntity<?> cancelMeeting(@RequestBody @Valid CancelTicket cancelTicket);
 
-    @PatchMapping("/update")
+    @PutMapping("/update")
     @Operation(summary = "Update meeting ticket")
     @PreAuthorize("hasRole('r:ticket:update')")
     ResponseEntity<?> updateMeeting(@RequestBody @Valid UpdateTicketInfo updateTicketInfo);
