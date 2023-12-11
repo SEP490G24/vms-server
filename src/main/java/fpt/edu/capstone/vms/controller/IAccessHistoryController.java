8b8e4121c789dfea9e730e959d0e1ad10dee4b27
Nghiem Xuan Trinh <xuantrinhxq2@gmail.com>
2023-12-12 00:04:18 +0700

    [REFACTOR] export

--- src/main/java/fpt/edu/capstone/vms/controller/IAccessHistoryController.java
+++ src/main/java/fpt/edu/capstone/vms/controller/IAccessHistoryController.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
